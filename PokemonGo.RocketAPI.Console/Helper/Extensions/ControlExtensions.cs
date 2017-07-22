85de1be5392fc621dda1d7603215cd46058a388b
Eduard Biete <esbiete@gmail.com>
2017-07-22 20:05:41 +0200

    Sync

---
+++ PokemonGo.RocketAPI.Console/Helper/Extensions/ControlExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+using System.Reflection;
+
+namespace PokeMaster
+{
+    public static class ControlExtensions
+    {
+        public static void DoubleBuffered(this Control control, bool enable)
+        {
+            var doubleBufferPropertyInfo = control.GetType().GetProperty("DoubleBuffered", BindingFlags.Instance | BindingFlags.NonPublic);
+            doubleBufferPropertyInfo.SetValue(control, enable, null);
+        }
+    }
+}
